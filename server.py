b7ca9881011e3970df40def8caafbc1a3a2cf575
KevinGg1008 <Kevinguegra@gmail.com>
2022-10-05 13:11:12 -0500

    Arreglos y rutas con flask

--- server.py
+++ server.py
@@ -1,6 +1,14 @@
 from flask import Flask,render_template
 app=Flask(__name__)
-
+nombre1='Spiderman Sin camino a casa'
 @app.route('/')
 def index():
     return render_template('Pagina_inicial.html')
+
+@app.route('/Cartelera/')
+def cartelera():
+     return render_template('Cartelera.html')
+
+@app.route('/Pelicula1/')
+def peli1():
+    return render_template('Pest_Pelicula1.html')
