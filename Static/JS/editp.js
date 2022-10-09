4b608a8d4d596fdfe916a32e27fa672b4e598f50
KevinGg1008 <Kevinguegra@gmail.com>
2022-10-09 12:02:57 -0500

    Integracion de base de datos

--- Static/JS/editp.js
+++ Static/JS/editp.js
@@ -1,19 +1,124 @@
-np
-function escribirnp(a,b){
+var titulo1
+var genero1
+var duracion1
+var sinopsis1
+var titulo_org1
+var reparto1
+var directo1
+function showp(){
+    np=document.getElementById("nmov").value;
+    console.log('Funciona')
+    switch (np){
+        case '1':
+            console.log('Entro')
+            document.getElementById("titulo").innerHTML=this.titulo1;
+            document.getElementById("generos").innerHTML=this.genero1;
+            document.getElementById("duracion").innerHTML=this.duracion1;
+            document.getElementById("sinopsis").innerHTML=this.sinopsis1;
+            document.getElementById("titulo_org").innerHTML=this.titulo_org1;
+            document.getElementById("reparto").innerHTML=this.reparto1;
+            document.getElementById("director").innerHTML=this.directo1;
+           
+            break;
+        case '2':
+            titulo2=document.getElementById("titulo").value;
+            genero2=document.getElementById("generos").value;
+            duracion2=document.getElementById("duracion").value;
+            sinopsis2=document.getElementById("sinopsis").value;
+            titulo_org2=document.getElementById("titulo_org").value;
+            reparto2=document.getElementById("reparto").value;
+            directo2=document.getElementById("director").value;
+            break;
+        case '3':
+            titulo3=document.getElementById("titulo").value;
+            genero3=document.getElementById("generos").value;
+            duracion3=document.getElementById("duracion").value;
+            sinopsis3=document.getElementById("sinopsis").value;
+            titulo_org3=document.getElementById("titulo_org").value;
+            reparto3=document.getElementById("reparto").value;
+            directo3=document.getElementById("director").value;
+            break;
 
+        case '4':
+            titulo4=document.getElementById("titulo").value;
+            genero4=document.getElementById("generos").value;
+            duracion4=document.getElementById("duracion").value;
+            sinopsis4=document.getElementById("sinopsis").value;
+            titulo_org4=document.getElementById("titulo_org").value;
+            reparto4=document.getElementById("reparto").value;
+            directo4=document.getElementById("director").value;
+            break;
+            
+        case '5':
+            titulo5=document.getElementById("titulo").value;
+            genero5=document.getElementById("generos").value;
+            duracion5=document.getElementById("duracion").value;
+            sinopsis5=document.getElementById("sinopsis").value;
+            titulo_org5=document.getElementById("titulo_org").value;               
+            reparto5=document.getElementById("reparto").value;
+            directo5=document.getElementById("director").value;
+            break;
+        case '6':
+            titulo6=document.getElementById("titulo").value;
+            genero6=document.getElementById("generos").value;
+            duracion6=document.getElementById("duracion").value;
+            sinopsis6=document.getElementById("sinopsis").value;
+            titulo_org6=document.getElementById("titulo_org").value;
+            reparto6=document.getElementById("reparto").value;
+            directo6=document.getElementById("director").value;
+            break;
+        case '7':
+            titulo7=document.getElementById("titulo").value;
+            genero7=document.getElementById("generos").value;
+            duracion7=document.getElementById("duracion").value;
+            sinopsis7=document.getElementById("sinopsis").value;
+            titulo_org7=document.getElementById("titulo_org").value;
+            reparto7=document.getElementById("reparto").value;
+            directo7=document.getElementById("director").value;
+            break;
+
+        case '8':
+            titulo8=document.getElementById("titulo").value;
+            genero8=document.getElementById("generos").value;
+            duracion8=document.getElementById("duracion").value;
+            sinopsis8=document.getElementById("sinopsis").value;
+            titulo_org8=document.getElementById("titulo_org").value;
+            reparto8=document.getElementById("reparto").value;
+            directo8=document.getElementById("director").value;
+            break;
+        case '9':
+            titulo9=document.getElementById("titulo").value;
+            genero9=document.getElementById("generos").value;
+            duracion9=document.getElementById("duracion").value;
+            sinopsis9=document.getElementById("sinopsis").value;
+            titulo_or9=document.getElementById("titulo_org").value;
+            reparto9=document.getElementById("reparto").value;
+            directo9=document.getElementById("director").value;
+            break;
+        case '10':
+            titulo10=document.getElementById("titulo").value;
+            genero10=document.getElementById("generos").value;
+            duracion10=document.getElementById("duracion").value;
+            sinopsis10=document.getElementById("sinopsis").value;
+            titulo_org10=document.getElementById("titulo_org").value;
+            reparto10=document.getElementById("reparto").value;
+            directo10=document.getElementById("director").value;
+            break;
+    }
 }
+
 function agp(){
+    np=document.getElementById("nmov").value;
     console.log("entro: "+np)
     switch (np){
         case '1':
-            titulo1=document.getElementById("titulo").value;
-            genero1=document.getElementById("generos").value;
-            duracion1=document.getElementById("duracion").value;
-            sinopsis1=document.getElementById("sinopsis").value;
-            titulo_org1=document.getElementById("titulo_org").value;
-            reparto1=document.getElementById("reparto").value;
-            directo1=document.getElementById("director").value;
-            console.log(titulo1);
+            this.titulo1=document.getElementById("titulo").value;
+            this.genero1=document.getElementById("generos").value;
+            this.duracion1=document.getElementById("duracion").value;
+            this.sinopsis1=document.getElementById("sinopsis").value;
+            this.titulo_org1=document.getElementById("titulo_org").value;
+            this.reparto1=document.getElementById("reparto").value;
+            this.directo1=document.getElementById("director").value;
             break;
         case '2':
             titulo2=document.getElementById("titulo").value;
--- server.py
+++ server.py
@@ -1,17 +1,28 @@
 from flask import Flask,render_template
+import sqlite3
+from sqlite3 import Error
+from db import get_db,close_db
 app=Flask(__name__)
-nombre1='Spiderman Sin camino a casa'
+
 @app.route('/')
 def index():
     return render_template('Pagina_inicial.html')
 
 @app.route('/Cartelera/')
 def cartelera():
-     return render_template('Cartelera.html')
+    return render_template('Cartelera.html')
 
-@app.route('/Pelicula1/')
-def peli1():
-    return render_template('Pest_Pelicula1.html')
+@app.route('/Inises/')
+def inises():
+    return render_template('Inicio_sesion.html')
+
+@app.route('/Registro/')
+def regis():
+    return render_template('Registro.html')
+
+@app.route('/Opinion/')
+def opinion():
+    return render_template('opinion.html')
 
 @app.route('/Agregarpelicula/')
 def Agpelicula():
@@ -20,3 +31,31 @@ def Agpelicula():
 @app.route('/Gestionarpelicula/')
 def Gpeliculas():
     return render_template('Gestionarpelicula.html')
+
+@app.route('/Gestinarusuarios/')
+def Gusuar():
+    return render_template('gestinarusuarios.html')
+
+@app.route('/Pelicula1/')
+def peli1():
+    db=get_db()
+
+    Datos=db.execute('Select * from Gpeliculas where ID=1').fetchone()
+    print(Datos)
+    name=Datos[1]
+    org=Datos[2]
+    gen=Datos[3]
+    dur=Datos[4]
+    res=Datos[5]
+    rep=Datos[6]
+    return render_template('Pest_Pelicula1.html',Datos=Datos)
+
+@app.route('/Pelicula2/')
+def peli2():
+    return render_template('Pest_Pelicula2.html')
+
+@app.route('/Pelicula3/')
+def peli3():
+    return render_template('Pest_Pelicula3.html')
+   
+    
