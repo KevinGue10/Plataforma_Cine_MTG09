3f5229721989208446d51016493114ea13b17afd
KevinGg1008 <Kevinguegra@gmail.com>
2022-10-11 17:57:18 -0500

    Cambios con flask en html

--- server.py
+++ server.py
@@ -2,25 +2,15 @@ from flask import Flask,render_template
 import sqlite3
 from sqlite3 import Error
 from db import get_db,close_db
+from quer import nomb
 
 app=Flask(__name__)
 
 @app.route('/')
 def index():
     db=get_db()
-    snm=['','','','','']
-    nm=['','','','','']
-    for i in range (5):
-        print(i)
-        a=str(i+1)
-        snm[i]=db.execute('Select name from Gpeliculas where ID='+a).fetchone()
-    print(snm)
-#    for i in range (5):
- #       x = snm[i].split("('")
-  #      nm[i]=x[1].split("',)")
-
-    print(nm)
-    return render_template('Pagina_inicial.html',nm=snm)
+    nm=nomb(db)
+    return render_template('Pagina_inicial.html',nm=nm)
 
 @app.route('/Cartelera/')
 def cartelera():
@@ -52,18 +42,21 @@ def Gusuar():
 
 @app.route('/Pelicula1/')
 def peli1():
+    
     db=get_db()
+    nm=nomb(db)
     Datos=db.execute('Select * from Gpeliculas where ID=1').fetchone()
     print(Datos)
-    return render_template('Pest_Pelicula1.html',Datos=Datos)
+    
+    return render_template('rutas.html',Datos=Datos,p=1,nm=nm)
 
 @app.route('/Pelicula2/')
 def peli2():
     db=get_db()
+    nm=nomb(db)
     Datos=db.execute('Select * from Gpeliculas where ID=2').fetchone()
     print(Datos)
-    ruta="{{url_for('static',filename='/image/Admin/Principales en cartelera/P2/Poster.jpg')}}"
-    return render_template('Pest_Pelicula1.html',Datos=Datos, ruta=ruta)
+    return render_template('rutas.html',Datos=Datos,p=2, nm=nm)
 
 @app.route('/Pelicula3/')
 def peli3():
@@ -82,8 +75,11 @@ def peli4():
 @app.route('/Pelicula5/')
 def peli5():
     db=get_db()
-    Datos=db.execute('Select * from Gpeliculas where ID=3').fetchone()
+    Datos=db.execute('Select * from Gpeliculas where ID=5').fetchone()
     print(Datos)
     return render_template('Pest_Pelicula1.html',Datos=Datos)   
-   
+
+#Querys DB
+
+
     
