ce351b9502e6a101df4e575a40f471ac10e4c86b
KevinGue10 <111770828+KevinGue10@users.noreply.github.com>
2022-10-14 18:54:50 -0500

    servir

--- server.py
+++ server.py
@@ -1,7 +1,7 @@
-from flask import Flask,render_template,redirect,request,url_for, session,flash
+from flask import Flask,render_template,request,redirect,url_for
 import os
 import sqlite3
-from form import Inicio,Registro
+from sqlite3 import Error
 from db import get_db,close_db
 from quer import nomb,gene
 from form_pel import FormPel
